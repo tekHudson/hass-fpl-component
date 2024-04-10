ae3547c8c17d8bf1a17c156569241d06f3fc67c9
Tek Hudson <tek.hudson@gmail.com>
2024-04-09 22:31:32 -0400

    Remove sample integration

--- custom_components/integration_blueprint/const.py
+++
@@ -1,9 +0,0 @@
-"""Constants for custom_components/hass-fpl-component."""
-from logging import Logger, getLogger
-
-LOGGER: Logger = getLogger(__package__)
-
-NAME = "HASS FPL Component"
-DOMAIN = "custom_components/hass-fpl-component"
-VERSION = "0.0.0"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
